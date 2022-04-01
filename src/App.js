912541b0c9623f9bce62fd1f8919892e7c8c53bf
stuartambient <sambient@gmail.com>
2022-04-01 06:20:16 -0600

    not sure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import ApiForm from './components/ApiForm';
 import InfiniteList from './components/InfiniteList';
--- src/components/Configuration.js
+++ src/components/Configuration.js
@@ -3,7 +3,19 @@ import '../styles/Configuration.css';
 
 function Configuration({ updateResults }) {
   console.log('ur: ', updateResults);
-  return <div className='configure'></div>;
+  return (
+    <div className='configure'>
+      <ul className='update-results'>
+        {/*  {updateResults.map(ur => {
+          return (
+            <li>
+              📂 {ur.path} 📋 {ur.msg}
+            </li>
+          );
+        })} */}
+      </ul>
+    </div>
+  );
 }
 
 export default Configuration;
